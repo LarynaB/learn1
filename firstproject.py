cbc858aee2353636bbda86731433d4635961db39
Twilight <lbillingh@gmail.com>
2021-11-21 10:59:36 -0800

    Update firstproject.py

--- firstproject.py
+++ firstproject.py
@@ -1,15 +1,27 @@
 #initialize variable
+theMatrix = ""
+system = ""
+neo = ""
+enemy = ""
+inside = ""
+save = ""
+unplugged = ""
+fight = ""
+
+profession = ["","","",""]
+adj = ["",""]
+
 
 #get input from user
 
 #initialize story
-madlibsStory = ("The Matrix is a system, Neo. That system is our enemy." +
-            "But when you're inside, you look around, what do you see?" +
-            "Businessmen, teachers, lawyers, carpenters. The very minds" +
-            "of the people we are trying to save. But until we do," +
-            "these people are still a part of that system and that makes" +
-            "them our enemy. You have to understand, most of these people" +
-            "are not ready to be unplugged. And many of them are so inured," +
-            "so hopelessly dependent on the system, that they will fight to protect it.")
+madlibsStory = (f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}." +
+            f"But when you're {inside}, you look around, what do you see?" +
+            f"{profession[0]},{profession[1]},{profession[2]},{profession[3]}. The very minds" +
+            f"of the people we are trying to {save}. But until we do," +
+            f"these people are still a part of that {system} and that makes" +
+            f"them our {enemy}. You have to understand, most of these people" +
+            f"are not ready to be {unplugged}. And many of them are so {adj[0]}," +
+            f"so hopelessly {adj[1]} on the {system}, that they will {fight} to protect it.")
 #print story
 print(madlibsStory)
